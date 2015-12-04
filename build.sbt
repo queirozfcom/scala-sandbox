3a9c65278bcab338d19144c11e33b975ccfd25eb
felipe <falmeida1988@gmail.com>
2015-12-04 01:04:12 -0200

    especially for chetan

--- build.sbt
+++ build.sbt
@@ -6,3 +6,4 @@ scalaVersion := "2.11.7"
 
 
 libraryDependencies += "org.scalatest" % "scalatest_2.11" % "2.2.4" % "test"
+libraryDependencies += "com.sksamuel.elastic4s" %% "elastic4s-core" % "2.1.0"
---
+++ src/main/scala/com/queirozf/sandbox/Elastic4s.scala
@@ -0,0 +1,34 @@
+package com.queirozf.sandbox
+
+import com.sksamuel.elastic4s.ElasticClient
+import org.elasticsearch.common.settings.Settings
+import com.sksamuel.elastic4s.ElasticDsl._
+import scala.collection.JavaConverters._
+import scala.concurrent.Await
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+
+/**
+ * Created by felipe on 03/12/15.
+ */
+object Elastic4s {
+
+  def main(args: Array[String]) {
+    val client = ElasticClient.remote("localhost",9300)
+
+    Await.result(
+      client.execute {
+        search in "movies" query "*" aggs {
+          aggregation terms "count_by_director" field "director"
+        }
+      } map { result =>
+        result.aggregations.asScala.foreach { agg =>
+          // agg is something that implements org.elasticsearch.search.aggregations.Aggregation
+          // lots of classes implement it, depending upon the type of aggregation you have got
+          println(s"\n ${agg.getClass} \n")
+          // the above will print org.elasticsearch.search.aggregations.bucket.terms.StringTerms
+        }
+      }, 10.seconds)
+  }
+
+}
