5ae0706f0027bb8cb8a56a227c0e32195394ac97
felipe <falmeida1988@gmail.com>
2015-12-25 04:16:36 -0200

    new stuff

--- build.sbt
+++ build.sbt
@@ -4,6 +4,9 @@ version := "1.0"
 
 scalaVersion := "2.11.7"
 
-
 libraryDependencies += "org.scalatest" % "scalatest_2.11" % "2.2.4" % "test"
 libraryDependencies += "com.sksamuel.elastic4s" %% "elastic4s-core" % "2.1.0"
+libraryDependencies +=  "com.typesafe.slick" %% "slick" % "3.1.1"
+libraryDependencies += "org.postgresql" % "postgresql" % "9.3-1104-jdbc4"
+libraryDependencies += "mysql" % "mysql-connector-java" % "5.1.6"
+libraryDependencies +=  "org.slf4j" % "slf4j-nop" % "1.6.4"
---
+++ src/main/scala/com/queirozf/sandbox/Slick3DynamicSchema.scala
@@ -0,0 +1,45 @@
+package com.queirozf.sandbox
+
+import slick.driver.MySQLDriver.api._
+import slick.profile.SqlProfile.ColumnOption.SqlType
+import scala.concurrent.ExecutionContext.Implicits.global
+import java.sql.Timestamp
+
+/**
+ * Created by felipe on 25/12/15.
+ */
+object Slick3DynamicSchema {
+
+  final case class User(id: Long, name: String, email: Option[String] = None, created: Timestamp, updated: Timestamp)
+
+  class Users(tag: Tag) extends Table[User](tag, "users") {
+
+    def id = column[Long]("id", O.PrimaryKey)
+
+    def name = column[String]("name")
+
+    def email = column[Option[String]]("email")
+
+    def created = column[Timestamp]("created", SqlType("timestamp not null default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP"))
+
+    def updated = column[Timestamp]("updated", SqlType("timestamp not null default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP"))
+
+    def * = (id, name, email, created, updated) <>(User.tupled, User.unapply)
+
+  }
+
+
+  def main(args: Array[String]) {
+
+    val db = Database.forURL("jdbc:mysql://localhost:3306/slickexamples?user=root&password=root")
+
+    val users = TableQuery[Users]
+
+    try {
+
+      db.run(users.schema.create)
+
+    } finally db.close()
+
+  }
+}
