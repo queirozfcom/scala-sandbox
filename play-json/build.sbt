b39a99abdb8f8e3aab74dd451d6560e65e304ed7
Felipe Almeida <felipe.almeida@vtex.com.br>
2016-03-09 20:41:09 -0300

    new commit (automatic message)

---
+++ play-json/build.sbt
@@ -0,0 +1,7 @@
+name := "play-json"
+
+version := "1.0"
+
+scalaVersion := "2.11.7"
+
+libraryDependencies += "com.typesafe.play" %% "play-json" % "2.5.0"
---
+++ play-json/src/main/scala/App.scala
@@ -0,0 +1,33 @@
+import play.api.libs.json._
+
+/**
+  * Created by felipe.almeida@vtex.com.br on 3/9/16.
+  */
+object App {
+
+  case class Person(name: String, age: Int, numChildren: Int)
+
+  implicit val pf:Reads[Person] = Json.reads[Person]
+
+  def main(args: Array[String]) {
+
+
+
+    val str =
+      """{
+      "name":"John",
+      "age":20,
+      "numChildren": 0
+      }"""
+
+    val json = Json.parse(str)
+
+    val res = json.validate[Person] match {
+      case s: JsSuccess[Person] => "ok"
+      case e: JsError => Json.prettyPrint(JsError.toJson(e))
+    }
+
+    println(res)
+  }
+
+}
