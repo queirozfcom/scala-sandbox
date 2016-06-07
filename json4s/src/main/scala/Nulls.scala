9750c7e33e86a22fcaa3c2618129793f6287fb18
Felipe Almeida <felipe.almeida@vtex.com.br>
2016-06-07 19:01:48 -0300

    new commit (automatic message)

--- json4s/src/main/scala/Nulls.scala
+++ json4s/src/main/scala/Nulls.scala
@@ -1,6 +1,6 @@
 import org.json4s._
 import org.json4s.JsonDSL._
-import org.json4s.jackson.Serialization.write
+import org.json4s.jackson.Serialization.{read,write}
 
 /**
   * Created by felipe.almeida@vtex.com.br on 07/06/16.
@@ -9,10 +9,10 @@ object Nulls extends App {
 
   implicit val formats = DefaultFormats
 
-  case class Person(name:String, maybeAge: Option[Int])
+  case class Person(name:String, age: Option[Int])
 
-  println(write(Person("john",None)))
-  //  prints {"name":"john"}
+  println(write(Person("john",None))) // prints {"name":"john"}
+  println(write(Person("mary",Some(20)))) // prints {"name":"mary","age":20}
 
   val noAge: Option[Int] = None
   val someAge:Option[Int] = Some(45)
@@ -22,5 +22,9 @@ object Nulls extends App {
 
   println(write(withNone)) // prints {"foo":"foo"}
   println(write(withSome)) // prints {"foo":"foo","age":45}
+
+
+  println(read[Person]("""{"name":"john"}""")) // no age provided
+  println(read[Person]("""{"name":"mary","age":20}""")) // age provided
 
 }
